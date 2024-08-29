f2958804976752151a2a735fe79ec68a224efb7f
jorge <esgerry81@gmail.com>
2024-08-29 12:09:38 -0600

    Se probo el funcionamiento de las clases en el main

--- src/main/java/org/example/EjemploJDBCUpdate.java
+++ src/main/java/org/example/EjemploJDBCUpdate.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.models.Categoria;
 import org.example.models.Producto;
 import org.example.repositorio.ProductoRepositorio;
 import org.example.repositorio.Repositorio;
@@ -9,7 +10,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 
-public class EjemploJDBCDelete {
+public class EjemploJDBCUpdate {
     public static void main(String[] args) {
         try (Connection conn = ConexionBaseDatos.getInstance()) {
 
@@ -21,10 +22,16 @@ public class EjemploJDBCDelete {
             System.out.println("========== obtener por id ==========");
             System.out.println(repositorio.porId(1L));
 
-            System.out.println("========== eliminar ==========");
-            repositorio.eliminar(8L);
-            repositorio.eliminar(9L);
-            repositorio.eliminar(10L);
+            System.out.println("========== eeditar producto ==========");
+            Producto producto = new Producto();
+            producto.setId(11L);
+            producto.setNombre("Teclado Razer Mecanico");
+            producto.setPrecio(700);
+            Categoria categoria = new Categoria();
+            categoria.setId(3L);
+            producto.setCategoria(categoria);
+            repositorio.guardar(producto);
+            System.out.println("Producto editado con exito");
             repositorio.getAll().forEach(System.out::println);
 
         }catch (SQLException e) {
